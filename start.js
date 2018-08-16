212a7e4c5ac4a01e56f599e62da8014b74bd5919
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2018-08-17 00:55:36 +0100

    added just now

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
 const time_past_in_words = (date, no_sec) => {
     const totalTime = typeof(date) === 'string'? new Date(date):date.getTime();
     const currentTime = new Date();
-
     const seconds = currentTime > totalTime ? (currentTime - totalTime) / 1000 : 0;
 
     if (seconds < 60) {
@@ -13,43 +12,27 @@ const time_past_in_words = (date, no_sec) => {
     } else {
         const minutes = seconds / 60;
         if (minutes < 60) {
-
             return Math.trunc(minutes) === 1 ? Math.trunc(minutes) + " minute ago" : Math.trunc(minutes) + " minutes ago";
-
         } else {
             const hours = minutes / 60;
             if (hours < 24) {
                 return Math.trunc(minutes) === 1 ? Math.trunc(hours) + " hour ago" : Math.trunc(hours) + " hours ago";
-
             } else {
                 const days = hours / 24;
                 if (days < 7) {
                     return Math.trunc(days) === 1 ? Math.trunc(days) + " day ago" : Math.trunc(days) + " days ago";
-
-                } else {
-
+                } else 
                     const weeks = days/7;
                     if(weeks < 4){
-                        return Math.trunc(weeks) === 1?Math.trunc(weeks) + " week ago":Math.trunc(weeks) + " weeks ago"
+                        return Math.trunc(weeks) === 1?Math.trunc(weeks) + " week ago":Math.trunc(weeks) + " weeks ago";
                     }else {
                         const months = weeks/4;
                     if(months < 12){
-                        return Math.trunc(months) === 1?Math.trunc(months) + " month ago":Math.trunc(months) + " months ago"
+                        return Math.trunc(months) === 1?Math.trunc(months) + " month ago":Math.trunc(months) + " months ago";
                     }else {
                         const years = months/12;
-                        
-                        return Math.trunc(years) === 1?Math.trunc(years) + " year ago":Math.trunc(years) + " years ago"
-
-                    }
-                    }
-
-                }
-            }
-        }
-    }
-
-};
-
+                        return Math.trunc(years) === 1?Math.trunc(years) + " year ago":Math.trunc(years) + " years ago";
+                    } }}}}};
 if(typeof module != 'undefined') {
     module.exports = time_past_in_words
   }
